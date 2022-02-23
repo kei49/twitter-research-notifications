06aed4f1f98a08692167234f6351d7924650e151
Kei Shimizu <suns.manage.k@gmail.com>
2022-02-23 16:28:45 +0900

    first commit for blockchain hackathon

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const baseConfig = {
+  token: process.env.BEARER_TOKEN,
+};
+
+export const twitterAPI = {
+  basePath: "https://api.twitter.com/2/",
+  searchRecentPath: "tweets/search/recent",
+  countsRecent: "tweets/counts/recent",
+};
---
+++ src/twitter/index.ts
@@ -0,0 +1,97 @@
+import axios from "axios";
+
+import { baseConfig, twitterAPI } from "../config";
+
+export default class TwitterClient {
+  private async get(path: string, params?: any) {
+    const url = twitterAPI.basePath + path;
+    const headers = { authorization: `Bearer ${baseConfig.token}` }
+
+    console.log(url, headers);
+
+    if (params) {
+      return await axios.get(url, { params, headers });
+    }
+
+    return await axios.get(url, { headers }); 
+  }
+
+  async searchRecentAPI(params: SearchParams) {
+    console.log("params: ", params);
+    const res = await this.get(twitterAPI.searchRecentPath, params);
+    const data: TweetsSearchData[] = res.data.data;
+    return data;
+  }
+
+  async countsRecentAPI(params: any) {
+    const res = await this.get(twitterAPI.countsRecent, params);
+    const _data: TweetsCountData = res.data;
+    const data = _data.data;
+    const totalTweetCount = _data.meta.total_tweet_count;
+
+    return { data, totalTweetCount };
+  }
+
+  async searchRecent(keywords: string, maxResults: number = 10, likeCountFilter: number = -1) {
+    const params: SearchParams = {
+      'query': `${keywords} has:links has:hashtags -is:retweet`,
+      'tweet.fields': 'author_id,public_metrics,text'
+    }
+
+    if (maxResults) {
+      params.max_results = maxResults;
+    }
+
+    let data = await this.searchRecentAPI(params);
+
+    if (likeCountFilter !== -1) {
+      data = data.filter(d => d.public_metrics.like_count > likeCountFilter);
+    }
+
+    console.log(data);
+    console.log("Number of data: ", data.length);
+    data.map((d: TweetsSearchData) => {
+      const tweetLink = `https://twitter.com/${d.author_id}/status/${d.id}`;
+      const publicMetrics = d.public_metrics;
+
+      console.log("LINK: ", tweetLink);
+      console.log("TEXT: ", d.text);
+      console.log(publicMetrics);
+    })
+  }
+
+  async countsRecent(params: any) {
+    const { data, totalTweetCount } = await this.countsRecentAPI(params);
+
+    console.log("block of counts: ", data.length);
+    console.log(data[0], data[data.length - 1]);
+
+    data.map(d => console.log(`count until ${d.end}: ${d.tweet_count}`));
+
+    console.log("total tweet count: ", totalTweetCount);
+  }
+}
+
+type TweetsCountData = {
+  data: {
+    end: string,
+    start: string,
+    tweet_count: number
+  }[],
+  meta: {
+    total_tweet_count: number
+  }
+}
+
+type TweetsSearchData = {
+  id: string,
+  author_id: string,
+  text: string,
+  public_metrics: any
+}
+
+type SearchParams = {
+  'query': string,
+  'tweet.fields': any,
+  'max_results'?: number
+}
